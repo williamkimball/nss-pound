a9a73a5494f1257ec6eb9aa74930638918686f10
William Kimball <wkkimball043@gmail.com>
2018-07-13 10:32:10 -0500

    finished ch6

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -6,30 +6,80 @@ import EmployeeList from "./EmployeeList";
 import Animal from "./Animal";
 import Employee from "./Employee";
 import Location from "./Location";
+import Login from "./Login";
 
 export default class ApplicationViews extends Component {
+  // Check if credentials are in local storage
+  isAuthenticated = () => localStorage.getItem("credentials") !== null;
+
   render() {
     return (
       <React.Fragment>
-        <Route exact path="/" component={LocationList} />
+        <Route
+          exact
+          path="/"
+          render={props => {
+            if (this.isAuthenticated()) {
+              return <LocationList />;
+            } else {
+              return <Login />;
+            }
+          }}
+        />
+        {/* <Route exact path="/locations" component={LocationList} /> */}
+        
         <Route
           path="/locations/:locationId"
           render={props => {
-            return <Location place={props.location.state.place} />;
+            if (this.isAuthenticated()) {
+              return <Location place={props.location.state.place} />;
+            } else {
+              return <Login />;
+            }
           }}
         />
-        <Route exact path="/animals" component={AnimalList} />
+        {/* <Route exact path="/animals" component={AnimalList} /> */}
+        <Route
+          exact
+          path="/animals"
+          render={props => {
+            if (this.isAuthenticated()) {
+              return <AnimalList />;
+            } else {
+              return <Login />;
+            }
+          }}
+        />
         <Route
           path="/animals/:animalId"
           render={props => {
-            return <Animal animal={props.location.state.animal} />;
+            if (this.isAuthenticated()) {
+              return <Animal place={props.location.state.animal} />;
+            } else {
+              return <Login />;
+            }
           }}
         />
-        <Route exact path="/employees" component={EmployeeList} />
+        {/* <Route exact path="/employees" component={EmployeeList} /> */}
+        <Route
+          exact
+          path="/employees"
+          render={props => {
+            if (this.isAuthenticated()) {
+              return <EmployeeList />;
+            } else {
+              return <Login />;
+            }
+          }}
+        />
         <Route
           path="/employees/:employeeId"
           render={props => {
-            return <Employee employee={props.location.state.employee} />;
+            if (this.isAuthenticated()) {
+              return <Employee employee={props.location.state.employee} />;
+            } else {
+              return <Login />;
+            }
           }}
         />
       </React.Fragment>
